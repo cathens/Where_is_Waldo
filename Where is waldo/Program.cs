0ed5badc9aa21999030931df552c8bfba59a38bc
Ben Carman <benthecarman@live.com>
2017-10-21 13:12:10 -0500

    f

--- Where is waldo/Program.cs
+++ Where is waldo/Program.cs
@@ -17,46 +17,53 @@ namespace Where_is_waldo
         {
 
             String win1 = "Test Window"; //The name of the window
+            String win2 = "Template";
 
 
 
-            String filepathA = "C:/Users/" + Environment.UserName.ToString() + "/Source/Repos/Where_is_Waldo/Where is waldo/11.png";
+            String filepathA = "C:/Users/" + Environment.UserName.ToString() + "/Source/Repos/Where_is_Waldo/Where is waldo/waldo.png";
             String filepathB = "C:/Users/" + Environment.UserName.ToString() + "/Source/Repos/Where_is_Waldo/Where is waldo/4.jpeg";
 
             CvInvoke.NamedWindow(win1, NamedWindowType.Normal); //Create the window using the specific name
+            CvInvoke.NamedWindow(win2, NamedWindowType.Normal); //Create the window using the specific name
 
 
-            Image<Bgr, byte> template1 = new Image<Bgr, byte>(filepathA); // Image A
-            Image<Bgr, byte> source = new Image<Bgr, byte>(filepathB); // Image B
-            Image<Bgr, byte> imageToShow = source.Copy();
+            Mat z = CvInvoke.Imread(filepathA, ImreadModes.Unchanged);
+            Image<Bgra, byte>  template1 = z.ToImage<Bgra, byte>();
+           
+         //   Image<Bgra, byte> template1 = new Image<Bgra, byte>(filepathA); // Image A
+            Image<Bgra, byte> source = new Image<Bgra, byte>(filepathB); // Image B
+            Image<Bgra, byte> imageToShow = source.Copy();
 
             double maxMatchValue = 0;
             Point maxPoint = new Point();
             Size maxSize = new Size();
 
-            for (double x = .1;x<1;x+=0.1) {
-                Image<Bgr, byte> template = template1.Resize(x, Inter.Linear);
-                using (Image<Gray, float> result = source.MatchTemplate(template, TemplateMatchingType.Ccoeff))
+            for (double x = .4;x<=1;x+=0.05) {
+                Image<Bgra, byte> template = template1.Resize(x, Inter.Linear);
+
+                using (Image<Gray, float> result = source.MatchTemplate(template, TemplateMatchingType.CcorrNormed))
                 {
                     double[] minValues, maxValues;
                     Point[] minLocations, maxLocations;
                     result.MinMax(out minValues, out maxValues, out minLocations, out maxLocations);
 
                     // You can try different values of the threshold. I guess somewhere between 0.75 and 0.95 would be good.
-                    if (maxValues[0] >= .9 && maxValues[0] > maxMatchValue)
+                    if (maxValues[0] >= .7 && maxValues[0] > maxMatchValue)
                     {
                         // This is a match. Do something with it, for example draw a rectangle around it.
                         maxMatchValue = maxValues[0];
                         maxPoint = maxLocations[0];
                         maxSize = template.Size;
 
-                        Console.WriteLine("" + maxMatchValue);
+                        Console.WriteLine("" + maxMatchValue+ " "+x);
+                        CvInvoke.Imshow(win2, template);
                     }
                 }
             }
 
             Rectangle match = new Rectangle(maxPoint, maxSize);
-            imageToShow.Draw(match, new Bgr(Color.Red), 3);
+            imageToShow.Draw(match, new Bgra(0,0,255,255), 3);
             //ImageBox imageBox1 = new ImageBox();
 
             // Show imageToShow in an ImageBox (here assumed to be called imageBox1)
