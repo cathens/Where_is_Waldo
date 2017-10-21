25d988350636e90e2f985cc17ee4fb4c7fadd195
Ben Carman <benthecarman@live.com>
2017-10-20 22:48:31 -0500

    pic

--- Where is waldo/Program.cs
+++ Where is waldo/Program.cs
@@ -20,17 +20,20 @@ namespace Where_is_waldo
             Mat img = new Mat(200, 400, DepthType.Cv8U, 3); //Create a 3 channel image of 400x200
             img.SetTo(new Bgr(255, 0, 0).MCvScalar); // set it to Blue color
 
+            Mat im = CvInvoke.Imread("C:/Users/benth/Source/Repos/Where_is_Waldo/Where is waldo/waldo.png");
+     
+
             //Draw "Hello, world." on the image using the specific font
             CvInvoke.PutText(
                img,
                "Hello, world",
-               new System.Drawing.Point(10,10),
+               new System.Drawing.Point(100,100),
                FontFace.HersheyComplex,
                1.0,
                new Bgr(0, 255, 0).MCvScalar);
 
 
-            CvInvoke.Imshow(win1, img); //Show the image
+            CvInvoke.Imshow(win1, im); //Show the image
             CvInvoke.WaitKey(0);  //Wait for the key pressing event
             CvInvoke.DestroyWindow(win1); //Destroy the window if key is pressed
         }
