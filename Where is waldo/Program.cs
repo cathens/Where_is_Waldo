97335ce5f298088d761549dde3a8369185ee0373
Ben Carman <benthecarman@live.com>
2017-10-20 19:46:27 -0500

    Add project files.

---
+++ Where is waldo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Where_is_waldo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
