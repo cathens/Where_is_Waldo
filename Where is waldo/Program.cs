d7f71bd2b8e5e6f1404c649a2502a796037bf639
Ben Carman <benthecarman@live.com>
2017-10-21 17:34:48 -0500

    coe

--- Where is waldo/Program.cs
+++ Where is waldo/Program.cs
@@ -22,8 +22,8 @@ namespace Where_is_waldo
 
 
 
-            String filepathA = "C:/Users/" + Environment.UserName.ToString() + "/Source/Repos/Where_is_Waldo/Where is waldo/Pics/waldoblue.png";
-            String filepathB = "C:/Users/" + Environment.UserName.ToString() + "/Source/Repos/Where_is_Waldo/Where is waldo/Pics/4.jpeg";
+            String filepathA = "C:/Users/" + Environment.UserName.ToString() + "/Source/Repos/Where_is_Waldo/Where is waldo/Pics/waldoblueactual1.png";
+            String filepathB = "C:/Users/" + Environment.UserName.ToString() + "/Source/Repos/Where_is_Waldo/Where is waldo/Pics/IMG_20212.png";
 
             CvInvoke.NamedWindow(win1, NamedWindowType.Normal); //Create the window using the specific name
             CvInvoke.NamedWindow(win2, NamedWindowType.Normal); //Create the window using the specific name
@@ -43,29 +43,31 @@ namespace Where_is_waldo
 
             for(int x1 = 0; x1 < bitmap.Width; x1++)
             {
-                for(int y1 = 0; y1< bitmap.Height; y1++)
+                for (int y1 = 0; y1 < bitmap.Height; y1++)
                 {
-                   // Console.WriteLine(bitmap.GetPixel(x1, y1).GetHue()+" "+ bitmap.GetPixel(x1, y1).GetSaturation());
-                    if(((bitmap.GetPixel(x1,y1).GetHue() < 20 && bitmap.GetPixel(x1,y1).GetHue() > 340) && bitmap.GetPixel(x1,y1).GetSaturation() < .0)
-                        || (bitmap.GetPixel(x1, y1).GetHue() > 20 && bitmap.GetPixel(x1, y1).GetHue() < 340))//RED
-                       // if(bitmap.GetPixel(x1, y1).GetHue()
-                    {
-                        bitmap.SetPixel(x1, y1,Color.Yellow);
+                    Color color = bitmap.GetPixel(x1, y1);
+                    // Console.WriteLine(color.GetHue()+" "+ color.GetSaturation());
+                    //  if((((color.GetHue() < 20 && color.GetHue() > 340) && color.GetSaturation() < .0)|| (color.GetHue() > 20 && color.GetHue() < 340))//RED
+                    //    ||())//Brown
+                    if ((!(color.R > 150 && color.G < 90 && color.B < 90))//RED
+                        && ((color.R > 60 && color.G > 60 && color.B > 60) || color.B - color.R > 20)//Black
+                            //&& (!(color.R > 210 && color.R < 230 && color.G > 187 && color.G < 203 && color.B > 170 && color.B < 190 && (color.R - color.B > 35) && (color.R - color.G > 20)))) { //Waldo face
+                            && !((color.R - color.B > 35) && (color.R - color.G > 20))){
+                bitmap.SetPixel(x1, y1, Color.Yellow);
                     }
                 }
             }
 
             Image<Bgr, Byte> myImage = new Image<Bgr, Byte>(bitmap);
-            CvInvoke.Imshow(win3, myImage);
 
 
             double maxMatchValue = 0;
             Point maxPoint = new Point();
             Size maxSize = new Size();
+            
+            for (double x = .05;x<=.4;x+=0.1) {
 
-            for (double x = .1;x<=1;x+=0.2) {
-
-                for (double y = .1; y <= 1; y += 0.2)
+                for (double y = .05; y <= .4; y += 0.1)
                 {
                     Image<Bgr, byte> template = template1.Resize((int)(template1.Width * x), (int)(template1.Height * y), Inter.Linear);
                     Console.WriteLine(x + " "+ y);
@@ -79,7 +81,7 @@ namespace Where_is_waldo
                         Console.WriteLine(maxValues[0]);
 
                         // You can try different values of the threshold. I guess somewhere between 0.75 and 0.95 would be good.
-                        if (maxValues[0] >= .7 && maxValues[0] > maxMatchValue)
+                        if (maxValues[0] >= .6 && maxValues[0] > maxMatchValue)
                         {
                             // This is a match. Do something with it, for example draw a rectangle around it.
                             maxMatchValue = maxValues[0];
@@ -92,17 +94,20 @@ namespace Where_is_waldo
                     }
                 }
             }
-
+            
             Rectangle match = new Rectangle(maxPoint, maxSize);
             imageToShow.Draw(match, new Bgr(0,0,255), 3);
-            //ImageBox imageBox1 = new ImageBox();
+            myImage.Draw(match, new Bgr(0, 0, 255), 3);
+            
+            ImageBox imageBox1 = new ImageBox();
 
-            // Show imageToShow in an ImageBox (here assumed to be called imageBox1)
-            //imageBox1.Image = imageToShow;
+             //Show imageToShow in an ImageBox (here assumed to be called imageBox1)
+             imageBox1.Image = imageToShow;
 
 
-            //imageToShow = imageToShow.Resize(.5, Inter.Linear);
+           
             CvInvoke.Imshow(win1, imageToShow); //Show the image
+            CvInvoke.Imshow(win3, myImage);
             CvInvoke.WaitKey(0);
             CvInvoke.DestroyWindow(win1);
         }
