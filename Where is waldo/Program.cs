611ab471cb6d0cdf4c45532a96d064965346a4d4
Ben Carman <benthecarman@live.com>
2017-10-21 01:34:17 -0500

    k

--- Where is waldo/Program.cs
+++ Where is waldo/Program.cs
@@ -20,58 +20,55 @@ namespace Where_is_waldo
 
 
 
-            String filepathA = "C:/Users/" + Environment.UserName.ToString() + "/Source/Repos/Where_is_Waldo/Where is waldo/waldo.png";
-            String filepathB = "C:/Users/" + Environment.UserName.ToString() + "/Source/Repos/Where_is_Waldo/Where is waldo/waldo.png";
+            String filepathA = "C:/Users/" + Environment.UserName.ToString() + "/Source/Repos/Where_is_Waldo/Where is waldo/11.png";
+            String filepathB = "C:/Users/" + Environment.UserName.ToString() + "/Source/Repos/Where_is_Waldo/Where is waldo/4.jpeg";
 
             CvInvoke.NamedWindow(win1); //Create the window using the specific name
-            /*
-            Mat img = new Mat(200, 400, DepthType.Cv8U, 3); //Create a 3 channel image of 400x200
-            img.SetTo(new Bgr(255, 0, 0).MCvScalar); // set it to Blue color
-
-            Mat im = CvInvoke.Imread("C:/Users/claud/Source/Repos/Where_is_Waldo/Where is waldo/waldo.png");
-     
-
-            //Draw "Hello, world." on the image using the specific font
-            CvInvoke.PutText(
-               img,
-               "Hello, world",
-               new System.Drawing.Point(100,100),
-               FontFace.HersheyComplex,
-               1.0,
-               new Bgr(0, 255, 0).MCvScalar);
 
 
-            CvInvoke.Imshow(win1, im); //Show the image
-            CvInvoke.WaitKey(0);  //Wait for the key pressing event
-            CvInvoke.DestroyWindow(win1); //Destroy the window if key is pressed
-            */
-
-            Image<Bgr, byte> template = new Image<Bgr, byte>(filepathA); // Image A
+            Image<Bgr, byte> template1 = new Image<Bgr, byte>(filepathA); // Image A
             Image<Bgr, byte> source = new Image<Bgr, byte>(filepathB); // Image B
             Image<Bgr, byte> imageToShow = source.Copy();
 
-            //template = template.Resize(0.3, Inter.Linear);
+            LinkedList<double> list = new LinkedList<double>();
+            LinkedList<Point> points = new LinkedList<Point>();
+            LinkedList<double> size = new LinkedList<double>();
 
-            using (Image<Gray, float> result = source.MatchTemplate(template, TemplateMatchingType.Ccoeff))
-            {
-                double[] minValues, maxValues;
-                Point[] minLocations, maxLocations;
-                result.MinMax(out minValues, out maxValues, out minLocations, out maxLocations);
-
-                // You can try different values of the threshold. I guess somewhere between 0.75 and 0.95 would be good.
-                if (maxValues[0] >= 0.9)
+            for (double i = .1;i<1;i+=0.1) {
+                Image<Bgr, byte> template = template1.Resize(i, Inter.Linear);
+                using (Image<Gray, float> result = source.MatchTemplate(template, TemplateMatchingType.Ccoeff))
                 {
-                    // This is a match. Do something with it, for example draw a rectangle around it.
-                    Rectangle match = new Rectangle(maxLocations[0], template.Size);
-                    imageToShow.Draw(match, new Bgr(Color.Red), 3);
+                    double[] minValues, maxValues;
+                    Point[] minLocations, maxLocations;
+                    result.MinMax(out minValues, out maxValues, out minLocations, out maxLocations);
+
+                    // You can try different values of the threshold. I guess somewhere between 0.75 and 0.95 would be good.
+                    if (maxValues[0] >= 0)
+                    {
+                        // This is a match. Do something with it, for example draw a rectangle around it.
+                        list.AddFirst(maxValues[0]);
+                        points.AddFirst(maxLocations[0]);
+
+                        Console.WriteLine("" + maxValues[0]);
+                    }
                 }
             }
 
+            for(double e: list)
+            {
+
+            }
+
+
+            Rectangle match = new Rectangle(maxLocations[0], template.Size);
+            imageToShow.Draw(match, new Bgr(Color.Red), 3);
             //ImageBox imageBox1 = new ImageBox();
 
             // Show imageToShow in an ImageBox (here assumed to be called imageBox1)
             //imageBox1.Image = imageToShow;
 
+
+            imageToShow = imageToShow.Resize(.5, Inter.Linear);
             CvInvoke.Imshow(win1, imageToShow); //Show the image
             CvInvoke.WaitKey(0);
             CvInvoke.DestroyWindow(win1);
