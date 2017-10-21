1a003c64f407059d44efdf54affad7a27f19c8f0
cathens <cathens@iastate.edu>
2017-10-20 23:40:59 -0500

    MatchTemplate method

--- Where is waldo/Program.cs
+++ Where is waldo/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 using Emgu.CV;
 using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
+using Emgu.CV.UI;
 
 namespace Where_is_waldo
 {
@@ -15,12 +17,15 @@ namespace Where_is_waldo
         {
 
             String win1 = "Test Window"; //The name of the window
+            String filepathA = "C:/Users/claud/Source/Repos/Where_is_Waldo/Where is waldo/waldo.png";
+            String filepathB = "C:/Users/claud/Source/Repos/Where_is_Waldo/Where is waldo/waldo.png";
+            /*
             CvInvoke.NamedWindow(win1); //Create the window using the specific name
 
             Mat img = new Mat(200, 400, DepthType.Cv8U, 3); //Create a 3 channel image of 400x200
             img.SetTo(new Bgr(255, 0, 0).MCvScalar); // set it to Blue color
 
-            Mat im = CvInvoke.Imread("C:/Users/benth/Source/Repos/Where_is_Waldo/Where is waldo/waldo.png");
+            Mat im = CvInvoke.Imread("C:/Users/claud/Source/Repos/Where_is_Waldo/Where is waldo/waldo.png");
      
 
             //Draw "Hello, world." on the image using the specific font
@@ -36,6 +41,34 @@ namespace Where_is_waldo
             CvInvoke.Imshow(win1, im); //Show the image
             CvInvoke.WaitKey(0);  //Wait for the key pressing event
             CvInvoke.DestroyWindow(win1); //Destroy the window if key is pressed
+            */
+
+
+            Image<Bgr, byte> source = new Image<Bgr, byte>(filepathB); // Image B
+            Image<Bgr, byte> template = new Image<Bgr, byte>(filepathA); // Image A
+            Image<Bgr, byte> imageToShow = source.Copy();
+
+            using (Image<Gray, float> result = source.MatchTemplate(template, TemplateMatchingType.Ccoeff))
+            {
+                double[] minValues, maxValues;
+                Point[] minLocations, maxLocations;
+                result.MinMax(out minValues, out maxValues, out minLocations, out maxLocations);
+
+                // You can try different values of the threshold. I guess somewhere between 0.75 and 0.95 would be good.
+                if (maxValues[0] > 0.9)
+                {
+                    // This is a match. Do something with it, for example draw a rectangle around it.
+                    Rectangle match = new Rectangle(maxLocations[0], template.Size);
+                    imageToShow.Draw(match, new Bgr(Color.Red), 3);
+                }
+            }
+
+            //ImageBox imageBox1 = new ImageBox();
+
+            // Show imageToShow in an ImageBox (here assumed to be called imageBox1)
+            //imageBox1.Image = imageToShow;
+
+            CvInvoke.Imshow(win1, imageToShow); //Show the image
         }
     }
 }
