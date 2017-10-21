84c88daee99607d20a9e4dabc34911712ed41e95
Ben Carman <benthecarman@live.com>
2017-10-21 13:58:09 -0500

    Pics

--- Where is waldo/Program.cs
+++ Where is waldo/Program.cs
@@ -21,8 +21,8 @@ namespace Where_is_waldo
 
 
 
-            String filepathA = "C:/Users/" + Environment.UserName.ToString() + "/Source/Repos/Where_is_Waldo/Where is waldo/waldoblue.png";
-            String filepathB = "C:/Users/" + Environment.UserName.ToString() + "/Source/Repos/Where_is_Waldo/Where is waldo/4.jpeg";
+            String filepathA = "C:/Users/" + Environment.UserName.ToString() + "/Source/Repos/Where_is_Waldo/Where is waldo/Pics/waldoyellow.png";
+            String filepathB = "C:/Users/" + Environment.UserName.ToString() + "/Source/Repos/Where_is_Waldo/Where is waldo/Pics/4.jpeg";
 
             CvInvoke.NamedWindow(win1, NamedWindowType.Normal); //Create the window using the specific name
             CvInvoke.NamedWindow(win2, NamedWindowType.Normal); //Create the window using the specific name
@@ -34,6 +34,19 @@ namespace Where_is_waldo
             Image<Bgra, byte> template1 = new Image<Bgra, byte>(filepathA); // Image A
             Image<Bgra, byte> source = new Image<Bgra, byte>(filepathB); // Image B
             Image<Bgra, byte> imageToShow = source.Copy();
+
+
+            Bitmap bitmap = new Bitmap(filepathB);
+
+            for(int x1 = 0; x1 < bitmap.Width; x1++)
+            {
+                for(int y1 = 0; y1< bitmap.Height; y1++)
+                {
+                    Color color = bitmap.GetPixel(x1, y1);
+                    
+                }
+            }
+         
 
             double maxMatchValue = 0;
             Point maxPoint = new Point();
